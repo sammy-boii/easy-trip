8b0c7f8a3effc897f6c546f305ea2abe8c4ee7ba
sammy-boii <neupanesamrajya2005@gmail.com>
2025-03-17 00:26:41 +0545

    pr

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,16 +1,14 @@
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-const API_URL = process.env.API_URL || 'http://localhost:5000/api'
+// API URL based on current environment
+const API_URL =
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:5000/api'
+    : 'https://easy-trip-3z43.onrender.com/api'
 
 // API Endpoints
 const ENDPOINTS = {
   TRIPS: `${API_URL}/trips`,
   BOOKINGS: `${API_URL}/bookings`
 }
-
-console.log(window.location.hostname)
 
 // Export constants
 export { API_URL, ENDPOINTS }
