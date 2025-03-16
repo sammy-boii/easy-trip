32f0bd09be92b3d8c5a05cf0e7a958780fb7630f
sammy-boii <neupanesamrajya2005@gmail.com>
2025-03-17 00:13:31 +0545

    A

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,16 +1,14 @@
-// Determine if we're in production based on the URL
-const isProduction = window.location.hostname !== 'localhost'
+require('dotenv').config()
 
-// API URLs
-const API_URL = isProduction
-  ? 'https://easy-trip-3z43.onrender.com/api'
-  : 'http://localhost:5000/api'
+const API_URL = process.env.API_URL || 'http://localhost:5000/api'
 
 // API Endpoints
 const ENDPOINTS = {
   TRIPS: `${API_URL}/trips`,
   BOOKINGS: `${API_URL}/bookings`
 }
+
+console.log(window.location.hostname)
 
 // Export constants
 export { API_URL, ENDPOINTS }
