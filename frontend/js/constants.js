438de97a7aabf93068fa385289af5f1019f07b71
sammy-boii <neupanesamrajya2005@gmail.com>
2025-03-17 00:49:45 +0545

    final

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const API_URL =
   window.location.hostname === 'localhost' ||
   window.location.hostname === '127.0.0.1'
     ? 'http://localhost:5000/api'
-    : 'https://easy-trip-3z43.onrender.com/api'
+    : 'https://easy-trip-server.vercel.app/api'
 
 // API Endpoints
 const ENDPOINTS = {
